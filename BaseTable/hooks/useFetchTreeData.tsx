fa7bd1702635163c72f283fc8a0aade6e795fab0
Maxwellio <pfcmax@mail.ru>
2026-08-11 16:11:46 +0500

    Update useFetchTreeData.tsx

--- BaseTable/hooks/useFetchTreeData.tsx
+++ BaseTable/hooks/useFetchTreeData.tsx
@@ -1,26 +1,28 @@
 import { useCallback, useEffect, useState } from 'react';
 import { useApi } from '../../services/api';
 
-export const useFetchData = <T,> (
-    url:String,
-    id:String | Number | undefined,
-    mode:'lazy' | 'eager' = 'lazy'
-) => {
-    const api = useApi(); //обязательно использовать <AxiosProvider>
+export const useFetchData = <T,> (url:String, columnFilters: Record<string,any>, reRenderSignal:any) => {
+    const api = useApi();
     const [data, setData] = useState<T[]>([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
- 
+    
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchData = async (filters) => {
+            const newfilers = filters.reduce((acc, filter) => {
+                if (filter.value !== undefined && filter.value !== null && String(filter.value).trim() !== '') {
+                    acc[filter.id] = filter.value;
+                }
+                return acc;
+            }, {});
             setLoading(true);
             try {
-                if (mode === 'lazy' && id == null) {
-                    throw new Error('Для lazy-режима требуется rootId');
+                const params = {
+                    ...newfilers
                 }
-                const reqApi = mode === 'eager' ? `${url}` : `${url}/${id}`;
-                const responseData = (await api.get(reqApi)).data;
-                setData(mode === 'eager' ? responseData : Array.of(responseData));
+                const reqApi = String(url);
+                const data = (await api.get(reqApi, {params})).data;
+                setData(data);
                 setError(null);
             } catch(err){
                 setError(err.message);
@@ -28,31 +30,31 @@ export const useFetchData = <T,> (
                 setLoading(false);
             }
         };
-        fetchData();
-    }, [url, id, mode]);
+        fetchData(columnFilters);
+    }, [JSON.stringify(columnFilters), reRenderSignal]);
 
     const fetchChildren = useCallback(async (parentId,) => {
-        if (mode !== 'lazy') {
-            return;
-        }
         const reqApi = `${url}/${parentId}/children`;
         const children = (await api.get(reqApi)).data;
         setData(prev => {
             const updateNode = (nodes) =>{
-                return nodes.map(node => {         
+                return nodes.map(node => {
+                    // Если узел родительский - вставляем
                     if (node.id  === parentId && !node.hasLoaded){
                         return {...node, children: children, hasLoaded: true};
                     }
+                    // Обходим дерево в поиске родителя
                     if (node.children) {
-                        return {...node, children: updateNode(node.children), hasLoaded: true}
+                        return {...node, children: updateNode(node.children)}
                     }
+                    // Возврат узла если дошли до конца и не нашли куда вставить
                     return node;
                 })
             }
             return updateNode(prev);
         })        
         
-    }, [url, mode])
+    }, [])
 
     return {
         data,
@@ -61,3 +63,4 @@ export const useFetchData = <T,> (
         fetchChildren
     }
 };
+
