31b9b96030b04c812b7a9c0e0c6b16edfd5c55da
Maxwellio <pfcmax@mail.ru>
2026-08-11 16:15:06 +0500

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,9 @@
-export { BaseTable } from './src/BaseTable/BaseTable';
+export { BaseTable, ColumnFilter } from './src/BaseTable/BaseTable';
 export { BaseTreeTable } from './src/BaseTable/BaseTreeTable';
-export { ChangePasswordInput, CheckBox, DebouncedInput, DebouncedParamInput, DynamicDateList, DynamicDatePicker, DynamicParamSelect, DynamicSelect, ParameterSelector, PasswordInput, RangePicker, ReportSign, SwitchImput } from './src/Input/InputComponents';
+export { ChangePasswordInput, CheckBox, DebouncedInput, DebouncedParamInput, DynamicDateList, DynamicDatePicker, DynamicParamSelect, DynamicSelect, NativeDynamicSelect, ParameterSelector, PasswordInput, RangePicker, ReportSign, SwitchImput } from './src/Input/InputComponents';
 export { Navigation } from './src/Navigation/Navigation';
-export { AxiosProvider, useApi } from './services/api';
+export { AxiosProvider, useApi } from './src/services/api';
 export { ToastProvider, useToast } from './src/Toast/ToastContext';
 export { formatDate, getMonthName } from './src/utils/dateFormatter';
 export { FILTER_TYPES } from './src/utils/types';
+
